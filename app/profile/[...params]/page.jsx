654d4c22d78848d4cf2132c2adfa20df5d8b9f92
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-27 23:49:50 +0200

    updated

--- app/profile/[...params]/page.jsx
+++ app/profile/[...params]/page.jsx
@@ -1,10 +1,14 @@
 'use client'
 import React from 'react'
 import { useState } from'react';
+import { useRouter } from 'next/router';
 
 
-const RepoPage = ({params}) => {
+const RepoPage = () => {
     const [repos, setRepo] = useState([]);
+
+    const router = useRouter();
+    const { params } = router.query;
     // const  handleSubmit = async() => {
     //     if(search.length > 0){
     //         const res = await fetch(`https://api.github.com/users/ro35ert/repos`);
