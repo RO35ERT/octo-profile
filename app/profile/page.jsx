59699e0089782f0e0c8e38320840f6ac7e85553e
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-27 21:39:40 +0200

    updated

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 import { useState } from'react';
 import {FaStar,FaEye, FaCodeBranch} from "react-icons/fa";
 import Link from 'next/link';
+import { revalidatePath } from 'next/cache';
 
 const Profile = () => {
 
     const [search, setSearch] = useState("");
-    const [repo, setRepo] = useState("");
+    const [repos, setRepo] = useState([]);
     const [set, setSet] = useState(false);
 
     const handleSearchChange = (e) => {
@@ -15,18 +16,24 @@ const Profile = () => {
     };
 
     const  handleSubmit = async() => {
-        
+        const res = await fetch(`https://api.github.com/users/ro35ert/repos`);
+        const data = await res.json();
+        setRepo(data);
+        setSet(true);
     }
+
+    handleSubmit();
   return (
     <div className='bg-gray-900 text-gray-100'>
         <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 flex flex-col justify-center items-center">
-            <form action="" onSubmit={handleSubmit}>
+            <div>
                 <input value={search} onChange={handleSearchChange} className="bg-white h-10 px-5 rounded-tl-lg rounded-bl-lg text-sm focus:outline-none text-black" placeholder="Search" />
-                <button type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-tr-lg rounded-br-lg">search</button>
-            </form>
+                <button onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-tr-lg rounded-br-lg">search</button>
+            </div>
             <div className="bg-white w-2/4 mx-10 my-6 px-5 py-4 rounded-tl-lg rounded-bl-lg text-sm focus:outline-none text-black">
                 <div className="">
-                    <Link href='/'>
+                    {repos.map((repo)=>{
+                        <Link href='/'>
                         <div className="bg-blue-500 text-gray-300 mb-4 font-bold py-2 px-4 rounded-lg">
                             <h4 className='px-5 text-lg'>My blog post</h4>
                             <div className="flex justify-between items-center text-lg px-5 py-5">
@@ -45,7 +52,7 @@ const Profile = () => {
                             </div>
                         </div>
                     </Link>
-
+                    })}
                 </div>
             </div>
         </div>
