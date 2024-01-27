c7061b5912a0619bdd14db1a46c0463cdeb0cf74
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-27 20:43:27 +0200

    updated

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -3,10 +3,6 @@
 import React from 'react';
 import { useState } from'react';
 
-function handleSubmit(search){
-
-}
-
 const Search = ({}) => {
 
     const [search, setSearch] = useState("")
@@ -14,11 +10,14 @@ const Search = ({}) => {
     const handleSearchChange = (e) => {
     setSearch(e.target.value);
     };
-    
+
+    function handleSubmit(){
+        
+    }
+
   return (
     <div>
         <form action="" onSubmit={handleSubmit}>
-            <p>{search}</p>
             <input value={search} onChange={handleSearchChange} className="bg-white h-10 px-5 rounded-tl-lg rounded-bl-lg text-sm focus:outline-none text-black" placeholder="Search" />
             <button type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-tr-lg rounded-br-lg">search</button>
         </form>
