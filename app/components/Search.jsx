acd0066c2472e47a5aac8b95344ed63bbc2fcd05
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-27 19:04:04 +0200

    updated

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,11 +1,18 @@
+'use client'
+
 import React from 'react';
 
+function handleSubmit(){
+
+}
 
 const Search = () => {
   return (
     <div>
-        <input className="bg-white h-10 px-5 rounded-tl-lg rounded-bl-lg text-sm focus:outline-none text-black" placeholder="Search" />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-tr-lg rounded-br-lg">search</button>
+        <form action="" onSubmit={handleSubmit}>
+            <input className="bg-white h-10 px-5 rounded-tl-lg rounded-bl-lg text-sm focus:outline-none text-black" placeholder="Search" />
+            <button type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-tr-lg rounded-br-lg">search</button>
+        </form>
     </div>
   )
 }
