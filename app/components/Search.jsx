0f57116c12b52853df3e748ec5f4f3b25d04a8ee
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-27 19:01:03 +0200

    fixed the search

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+
 
 const Search = () => {
   return (
     <div>
-        <input className="border-2 border-gray-300 bg-white h-10 px-5 rounded-lg text-sm focus:outline-none text-black" placeholder="Search" />
+        <input className="bg-white h-10 px-5 rounded-tl-lg rounded-bl-lg text-sm focus:outline-none text-black" placeholder="Search" />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-tr-lg rounded-br-lg">search</button>
     </div>
   )
 }
 
-export default Search
+export default Search;
