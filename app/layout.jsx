2d367813055964cdeeb91acab064e366df92e459
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-27 19:28:18 +0200

    updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
+import Footer from "./footer/page";
 
 const roboto = Roboto(
   { 
@@ -18,9 +19,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={roboto.className}>
         <div className='bg-gray-900 text-gray-100'>
-            <NavBar/>
+            <NavBar className='bg-gray-900 text-gray-100'/>
         </div>  
         {children}
+        <div className='bg-gray-900 text-gray-100 py-10 px-8'>
+          <Footer/>
+        </div>
       </body>
     </html>
   );
