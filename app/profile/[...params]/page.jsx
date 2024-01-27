b0a47f5c90b9b717b91e745e5b837a3d258ab25f
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-28 00:59:54 +0200

    updated

--- app/profile/[...params]/page.jsx
+++ app/profile/[...params]/page.jsx
@@ -1,27 +1,30 @@
 'use client'
 import React from 'react'
 import { useState } from'react';
+import { useEffect } from 'react';
 
 
-  const RepoPage = ({params}) => {
-    const [repo, setRepo] = useState([]);
+  const RepoPage = async({params}) => {
+    const [repo, setRepo] = useState({});
+    const [isSet, setIsSet] = useState(false);
+
+
     const  getData = async() => {
         const res = await fetch(`https://api.github.com/users/${params.params[1]}/repos`);
         const data = await res.json();
-        setRepo(data);
-        // return data;
+        const filtered = data.filter(e => e.name === params.params[0]);
+        setRepo(filtered);
     }
 
-    // const handleData = async() => {
-    //   const data = await getData();
-    //   const filtered = data.filter(repo => repo.name === params.params[0]);
-    //   setRepo(filtered);
-    // }
+    await getData();
 
-   
-    console.log(repo);
+
+
+    console.log(isSet);
   return (
-    <div>{repo.name}</div>
+    <div>
+        {isSet? <div>{repo.name}</div> : <div>loading...</div>}
+    </div>
   )
 }
 
