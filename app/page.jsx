4448b44204bfedf9c6f2aaeaf17b644a26165bcb
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-27 19:32:56 +0200

    added bg image

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import NavBar from './components/NavBar';
 const HomePage = () => {
   return (
     <div className="">
-      
+      <img src={`https://unsplash.com/photos/blue-and-black-penguin-plush-toy-UT8LMo-wlyk`} alt="Github" />
     </div>
   );
 }
