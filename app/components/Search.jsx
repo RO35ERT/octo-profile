cd8976f0cadd8b61919b570847b0bcd201244fb7
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-27 18:51:44 +0200

    added search input

---
+++ app/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>
+        <input className="border-2 border-gray-300 bg-white h-10 px-5 rounded-lg text-sm focus:outline-none text-black" placeholder="Search" />
+    </div>
+  )
+}
+
+export default Search
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Search from '../components/Search'
 
 const Profile = () => {
   return (
-    <div className='bg-black text-gray-100'>Profile</div>
+    <div className='bg-black text-gray-100'>
+        <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 flex justify-center items-center">
+            < Search/>
+        </div>
+    </div>
   )
 }
 
