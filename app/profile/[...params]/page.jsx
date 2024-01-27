862b37750831c2d78a327a49d2dee4a0f238080f
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-28 00:24:13 +0200

    updated

--- app/profile/[...params]/page.jsx
+++ app/profile/[...params]/page.jsx
@@ -3,20 +3,25 @@ import React from 'react'
 import { useState } from'react';
 
 
-const RepoPage = ({params}) => {
-    const [repos, setRepo] = useState([]);
-    const  handleSubmit = async() => {
-        if(search.length > 0){
-            const res = await fetch(`https://api.github.com/users/${params.params[1]}/repos`);
-            const data = await res.json();
-            setRepo(data);
-        }
+  const RepoPage = ({params}) => {
+    const [repo, setRepo] = useState([]);
+    const  getData = async() => {
+        const res = await fetch(`https://api.github.com/users/${params.params[1]}/repos`);
+        const data = await res.json();
+        setRepo(data);
+        // return data;
     }
 
-    
+    // const handleData = async() => {
+    //   const data = await getData();
+    //   const filtered = data.filter(repo => repo.name === params.params[0]);
+    //   setRepo(filtered);
+    // }
 
+   
+    console.log(repo);
   return (
-    <div>{}</div>
+    <div>{repo.name}</div>
   )
 }
 
