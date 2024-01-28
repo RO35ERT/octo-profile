1fa1df49e285e940519c6864a847a934da5bbdac
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-28 12:24:10 +0200

    updated

--- app/profile/[...params]/page.jsx
+++ app/profile/[...params]/page.jsx
@@ -9,27 +9,53 @@ const  getData = async(params) => {
   return filtered[0];
 }
 
+const getDirs = async(params) => {
+
+  const res = await fetch(`https://api.github.com/users/${params.params[1]}/repos`);
+  const data = await res.json();
+  const filtered = data.filter(e => e.type === 'dir');
+  return filtered[0];
+
+}
+
   const RepoPage = async({params}) => {
     const repo = await getData(params);
+    const dirs = await getDirs(params);
   return (
     <div>
         <div className="mt-10">
             <div className="bg-blue-500 w-3/4 text-gray-300 mb-4 mx-auto font-bold py-2 px-4 rounded-lg">
-                      <h4 className='px-5 text-lg'>{repo.name}</h4>
-                      <div className="flex justify-between items-center text-lg px-5 py-5">
-                            <div className="flex items-center gap-4">
-                                <FaStar/>
-                                <p>{repo.stargazers_count}</p>
-                            </div>
-                            <div className="flex items-center gap-4">
-                                <FaCodeBranch/>
-                                <p>{repo.forks_count}</p>
-                            </div>
-                            <div className="flex items-center gap-4">
-                                <FaEye/>
-                                <p>{repo.watchers}</p>
-                            </div>
+                <h4 className='px-5 text-lg'>{repo.name}</h4>
+                <div className="flex justify-between items-center text-lg px-5 py-5">
+                      <div className="flex items-center gap-4">
+                        <FaStar/>
+                        <p>{repo.stargazers_count}</p>
                       </div>
+                      <div className="flex items-center gap-4">
+                        <FaCodeBranch/>
+                        <p>{repo.forks_count}</p>
+                      </div>
+                      <div className="flex items-center gap-4">
+                        <FaEye/>
+                        <p>{repo.watchers}</p>
+                      </div>
+                </div>
+                <div className="px-5 pb-4">
+                  <h3>Directories</h3>
+                  <div className="">
+                      <ul className="flex flex-col gap-4">
+                        {dirs.map((dir)=>{
+                          return (
+                            <li className="text-lg" key={dir.name}>
+                              <Link href={`/profile/${dir.name}/${params.params[1]}`}>
+                                <a className="text-blue-500">{dir.name}</a>
+                              </Link>
+                            </li>
+                          )
+                        })}
+                      </ul>
+                  </div>
+                </div>
             </div>
         </div>
     </div>
